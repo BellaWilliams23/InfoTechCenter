ce2f927ddbb57cccc4cfe8c6f8691390f57daf10
Williams <23678bella23678@gmail.com>
2024-02-21 13:49:26 -0500

    Added ... to string

--- GrimaceShake.py
+++ GrimaceShake.py
@@ -28,7 +28,7 @@ def gasLevelAlert():
         sleep(1.25)
         print("\n  ***Calling Triple AAA***")
     elif gasLevelIndicator == "Low":
-        print("Your gas tank is extremely low, checking Google Maps for the closest gas station")
+        print("Your gas tank is extremely low, checking Google Maps for the closest gas station...")
         sleep(2.5)
         print("The closest gas station is",listOfGasStations(), "which is",milesToGasStationsLow,"miles away.")
 
