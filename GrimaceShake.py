131708e725208a45912ecb8f6a43f70312c571ad
Williams <23678bella23678@gmail.com>
2024-02-26 13:45:13 -0500

    Defined the weather function- Called- Stable

---
+++ GrimaceShake.py
@@ -0,0 +1,16 @@
+print("\n*****************************************************\n")
+
+print("Weather Branch\n")
+
+#Import libraries here
+import random
+from time import sleep
+
+#Create a function that randomly chooses the weather from a list
+def weather():
+    weatherForecast = ["Snowing", "Blizards", "Raining", "Foggy", "Windy", "Icy", "Sunny"]
+    weatherConditions = random.choice(weatherForecast)
+    return weatherConditions
+
+
+print(weather())
